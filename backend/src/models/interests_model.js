e86a361f1b9e0ec6a4910dd83aacde1fbba8afa9
Roman Izegov <izegovrg@yandex.ru>
2026-04-25 14:20:33 +0300

    added interests model seeed and upgraded intersts table adding category

---
+++ backend/src/models/interests_model.js
@@ -0,0 +1,42 @@
+const db = require('../../db/db')
+
+const getAllInterests = () => {
+  return db('interests')
+  .orderBy('name', 'desc')
+}
+
+const addInterestToUser = async (user_id, interest_id) => {
+  const [addInterestUser] = await db ('user_interests')
+  .insert({user_id, interest_id })
+  .returning('*')
+  return addInterestUser
+}
+
+const addInterestToEvent = async(event_id, interest_id) => {
+  const [addInterestEvent] = await db('event_interests')
+  .insert({event_id, interest_id})
+  .returning("*")
+  return addInterestEvent
+}
+
+const getInterestByUserId = async (user_id) => {
+  return db('user_interests')
+  .join('interests', 'user_interests.interest_id', '=', 'interests.id')
+  .where({user_id})
+  .orderBy('name', 'desc')
+}
+
+const getInterestByEventId = async (event_id) => {
+  return db('event_interests')
+  .join('interests','event_interests.interest_id', 'interests.id' )
+  .where({event_id})
+  .orderBy('name', 'desc')
+}
+
+module.exports = {
+  addInterestToUser,
+  addInterestToEvent,
+  getAllInterests,
+  getInterestByEventId,
+  getInterestByUserId
+}
