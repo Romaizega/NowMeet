387805fa588f1362dd5ad8c21ead03e253497b54
Roman Izegov <izegovrg@yandex.ru>
2026-04-27 08:09:39 +0300

    added add to interest to event

--- backend/src/controllers/interests_controller.js
+++ backend/src/controllers/interests_controller.js
@@ -1,4 +1,5 @@
 const interestsModel = require('../models/interests_model')
+const eventModel = require('../models/events_model')
 
 const getAllInterests = async (req, res) => {
   try {
@@ -24,7 +25,30 @@ const addInterestUser = async (req, res) => {
   }
 }
 
+const addInterestEvent = async (req, res) => {
+  const {interest_id} = req.body
+  const creator_id = req.user.user_id
+  const {id} = req.params
+  if(!interest_id){
+    return res.status(400).json({message: "Interest not selected"})
+  }
+  try {
+    const event = await eventModel.getEventById(id)
+    if(!event){
+      return res.status(404).json({message: "The event not found"})
+    }
+    if(creator_id !== event.creator_id){
+      return res.status(403).json({message: "You can add interest only for your own events"})
+    }
+    const addInterestEvent = await interestsModel.addInterestToEvent(id, interest_id)
+    return res.status(201).json({message: "Interest added"})
+  } catch (error) {
+     return res.status(500).json({message: "Server error", error: error.message})    
+  }
+}
+
 module.exports = {
   getAllInterests,
-  addInterestUser
+  addInterestUser,
+  addInterestEvent
 }
--- backend/src/routers/interests_router.js
+++ backend/src/routers/interests_router.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const {getAllInterests, addInterestUser} = require('../controllers/interests_controller')
+const {getAllInterests, addInterestUser, addInterestEvent} = require('../controllers/interests_controller')
 const authenticateJWT = require('../middleware/auth_middleware')
 
 const router = express.Router()
 
 router.get('/all', getAllInterests)
 router.post('/user', authenticateJWT, addInterestUser)
+router.post('/event/:id', authenticateJWT, addInterestEvent)
 
 module.exports = router
