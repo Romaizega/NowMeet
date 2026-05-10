c00eecc5cbf77a2a446b34e7fb01713d192e0e06
Roman Izegov <izegovrg@yandex.ru>
2026-05-10 15:26:00 +0300

    added socket auth middleware

---
+++ backend/src/socket/socketAuth.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+
+const socketAuth = (socket, next) => {
+  const token = socket.handshake.auth.token
+
+  if(!token){
+    return next (new Error('Unauthorized'))
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    socket.user = decoded
+    next()
+  } catch (error) {
+    next(new Error("Invalid token"))
+    
+  }
+}
+
+module.exports = socketAuth
