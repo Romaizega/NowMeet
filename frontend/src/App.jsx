b6a9d979477112dc70be2b6e6bc166d612b41f53
Roman Izegov <izegovrg@yandex.ru>
2026-05-15 19:36:23 +0300

    init Header and Footer fixed issue Register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,11 +5,14 @@ import Profile from "./pages/Profile"
 import Home from "./pages/Home"
 import Events from "./pages/Events"
 import EventDetails from "./pages/EventDetails"
+import Header from "./components/Header"
+import Footer from "./components/Footer"
 
 function App() {
   return (
     <>
     <div>
+    <Header/>
     <Routes>
       <Route path="/" element= {<Home/>}/>
       <Route path="/register" element= {<Register/>}/>
@@ -18,6 +21,7 @@ function App() {
       <Route path="/events/:id" element= {<Events/>}/>
       <Route path="/event-details" element= {<EventDetails/>}/>
     </Routes>
+    <Footer/>
     </div>
     </>
   )
