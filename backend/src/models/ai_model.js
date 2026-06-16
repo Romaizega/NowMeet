e4f9659db2627e737b457e4469c2d0a9f93b1881
Roman Izegov <izegovrg@yandex.ru>
2026-06-16 11:19:57 +0300

    inited ai logic install openai comtroller model and prompt

---
+++ backend/src/models/ai_model.js
@@ -0,0 +1,33 @@
+const db = require("../../db/db");
+
+const getUserInterests = async (user_id) => {
+  return db("user_interests")
+    .join("interests", "user_interests.interest_id", "=", "interests.id")
+    .where({ user_id })
+    .select("interests.id", "interests.name")
+    .orderBy("name", "desc");
+};
+
+const getAllUsersWithInterests = async (user_id) => {
+  return db("users")
+    .join("user_interests", "users.id", "=", "user_interests.user_id")
+    .join("interests", "user_interests.interest_id", "=", "interests.id")
+    .whereNot("users.id", user_id)
+    .select("interests.*", 'users.id', "users.username", "users.photo", "users.created_at")
+};
+
+
+const getActiveEventsWithInterests = async () => {
+  return db('events')
+  .join('event_interests', 'events.id', '=', 'event_interests.event_id')
+  .join('interests', 'event_interests.interest_id', '=', 'interests.id')
+  .where('status', 'open')
+  .select('events.*','interests.id', 'interests.name')
+  .orderBy('name', 'desc')
+}
+
+ module.exports = {
+  getUserInterests,
+  getAllUsersWithInterests,
+  getActiveEventsWithInterests
+ }
