5dca9dc2eda60f413c0c41baf7220715bfa0fc57
Roman Izegov <izegovrg@yandex.ru>
2026-07-27 18:29:54 +0300

    added city match in AI

--- backend/src/models/ai_model.js
+++ backend/src/models/ai_model.js
@@ -3,8 +3,9 @@ const db = require("../../db/db");
 const getUserInterests = async (user_id) => {
   return db("user_interests")
     .join("interests", "user_interests.interest_id", "=", "interests.id")
+    .join("users", "users.id", "=", "user_interests.user_id")
     .where({ user_id })
-    .select("interests.id", "interests.name")
+    .select("interests.id", "interests.name", "users.city")
     .orderBy("name", "desc");
 };
 
@@ -13,21 +14,35 @@ const getAllUsersWithInterests = async (user_id) => {
     .join("user_interests", "users.id", "=", "user_interests.user_id")
     .join("interests", "user_interests.interest_id", "=", "interests.id")
     .whereNot("users.id", user_id)
-    .select("interests.*", 'users.id', "users.username", "users.photo", "users.created_at")
+    .select(
+      "interests.*",
+      "users.id",
+      "users.username",
+      "users.photo",
+      "users.created_at",
+      "users.city",
+    );
 };
 
-
 const getActiveEventsWithInterests = async () => {
-  return db('events')
-  .join('event_interests', 'events.id', '=', 'event_interests.event_id')
-  .join('interests', 'event_interests.interest_id', '=', 'interests.id')
-  .where('status', 'open')
-  .select('events.id as event_id','interests.id', 'interests.name', 'events.title', 'events.event_start', 'events.place_name')
-  .orderBy('name', 'desc')
-}
+  return db("events")
+    .join("event_interests", "events.id", "=", "event_interests.event_id")
+    .join("interests", "event_interests.interest_id", "=", "interests.id")
+    .where("status", "open")
+    .select(
+      "events.id as event_id",
+      "interests.id",
+      "interests.name",
+      "events.title",
+      "events.event_start",
+      "events.place_name",
+      "events.city",
+    )
+    .orderBy("name", "desc");
+};
 
- module.exports = {
+module.exports = {
   getUserInterests,
   getAllUsersWithInterests,
-  getActiveEventsWithInterests
- }
+  getActiveEventsWithInterests,
+};
